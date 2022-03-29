f6bfd80c77c771384e863390267a62f4e6b85410
doddonicotina <74264293+doddonicotina@users.noreply.github.com>
2022-03-29 12:19:32 +0200

    ok

--- Pila.h
+++ Pila.h
@@ -1,7 +1,23 @@
+
+
 template <typename T>
 class Pila{
-  
-  
+    private:
+        T *ptr;
+        int size;
+        int capacity;
+
+
+    public:
+        Pila(int capacity);
+        ~Pila();
+
+        T pop();
+        Void push(T elemento);
+
+        int getSize();
+        bool isFull;
+        bool isEmpty();
 
 
 };
