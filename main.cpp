d77e7909be6782d592f6c4a4178a8dd12d898a04
doddonicotina <74264293+doddonicotina@users.noreply.github.com>
2022-03-31 13:54:20 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,16 +1,27 @@
-#include <iostream>
 #include "Pila.cpp"
 int main(){
 
     {
         Pila <int> p(3);
-        int a=5;
-        p.push(a);
-        a++;
-        p.push(a);
-        a++;
-        p.push(a);
+        std::cout<<" empty? "<<p.isEmpty()<<"\n";
+        p.push(55);
+        p.push(88);
+        p.push(3);
+		std::cout<<" is full? "<<p.isFull()<<"\n";
+		int N = p.getSize();
+		for (int i=0; i<N; i++)
+		 {
+		 	std::cout<<"elem "<<i<<p.pop()<<" \n";
+		 }
 
-        cout<<p.pop()<<endl;
+        std::cout<<p.pop()<<std::endl;
     }
+    
+    {
+    	Pila<char> p(4);
+    	p.push('A');
+    	p.push('b');
+    	p.push('c');
+    	p.push('d');
+	}
 }
